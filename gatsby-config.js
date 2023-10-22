50adc402fa32e77c79116d5c9805d674c5a48939
Saul Belisle <rbelisle1270@gmail.com>
2023-10-22 09:42:36 -0600

    new deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
    * Gatsby has a rich ecosystem of plugins.
    * If you need any more you can search here: https://www.gatsbyjs.com/plugins/
    */
+  pathPrefix: "/wordpress-gatsby-start-v1",
   plugins: [
     {
       /**
